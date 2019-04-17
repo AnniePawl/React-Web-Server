7127552a4565c56c63a34bb7c388b1ff38176823
Anna Pawl <annampawl@gmail.com>
2019-04-17 14:12:12 -0700

    die rolling funcs in progress

--- server.js
+++ server.js
@@ -13,30 +13,57 @@ app.use(bodyParser.json())
 // Apparently this is expected behavior... **
 // Test this route with: localhost:4000/
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello World', foo: 'bar' })
+  res.json({ message: 'Get Ready to Roll Some Die' })
 })
 
 app.get('/rollDice', (req, res) => {
-  res.json({ 'message': 'rolled dice' })
+  res.json({ message: 'Die have been rolled' })
 })
 
-// A simple route that returns a JSON object
-// Test this route with:
+// Simple route that returns a JSON object
 app.get('/about', (req, res) => {
   // This Object is converted to JSON and returned.
   res.json({
-    about: 'this service generates a random numbers.',
+    about: 'This service generates a random numbers.',
     date: new Date().toLocaleString(),
   })
 })
 
-// Random number route
+// Random Whole Number Route
+// Returns a random whole number between 0 and n-1
 // Test this route with: http://localhost:4000/random?n=99
-// Where n=99 sets the range of the random number returned
 app.get('/random', (req, res) => {
   const { n } = req.query
   const value = random(n)
-  res.json({ value })
+  res.json({
+    message: 'Returning a Random Whole Number between 0 and n-1',
+    value,
+  })
+})
+
+// RollDie Route
+// Returns a random whole number between 0 and n-1
+// Test this route with: http://localhost:4000/random?n=99
+app.get('/rollDie', (req, res) => {
+  const { n } = req.query
+  const value = random(n)
+  res.json({
+    message: 'You see me rollin',
+    value,
+  })
+})
+
+
+// RandomRolls Route
+// Returns a random whole number between 0 and n-1
+// Test this route with: http://localhost:4000/random?n=99
+app.get('/randomRolls', (req, res) => {
+  const { n } = req.query
+  const value = random(n)
+  res.json({
+    message: 'Rollin Randoms',
+    value,
+  })
 })
 
 const port = 4000
--- utils.js
+++ utils.js
@@ -1,17 +1,19 @@
 /* eslint-disable semi */
 
 function random(n) {
-  return Math.random() * n
+  // Returns a random whole number between 0 and n-1
+  return Math.floor(Math.random() * n)
 }
 
 function randomD(n) {
-  
-  return 0
+  // Simulates the roll of a die and returns a whole number between 1 and n. Takes in one parameter, n 
+  return Math.floor(Math.random() * n)
 }
 
 function randomRolls(n, s) {
-
-  return []
+  // Generates a series of die rolls and returns an array of random numbers. Takes two parameters: n=number of die, s=number of sides per die. Return Value between 1 and s
+  rollsArray = []
+  return rollsArray
 }
 
 module.exports.random = random
